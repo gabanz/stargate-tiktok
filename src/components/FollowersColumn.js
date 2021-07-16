c9fdc9151f336fdd3f205928436b17a97da7e401
Faiz Azhar <faiz@cloudflare.com>
2021-07-17 00:45:26 +0800

    init

---
+++ src/components/FollowersColumn.js
@@ -0,0 +1,10 @@
+const FollowersColumn = () => {
+    return (
+      <div className="followers-column">
+          <h1>FollowersColumn</h1>
+      </div>
+    );
+  }
+  
+  export default FollowersColumn;
+  
